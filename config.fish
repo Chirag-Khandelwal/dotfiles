70b5a83d484d977c385b78347bbd54101881fd1d
Electrux <ElectruxRedsworth@gmail.com>
2024-09-29 15:58:44 -0700

    add abbreviations to config.fish

--- config.fish
+++ config.fish
@@ -1,8 +1,26 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
+
+	# Git commands
+	abbr --add ga 'git add'
+	abbr --add gb 'git branch'
+	abbr --add gc 'git commit'
+	abbr --add gd 'git diff'
+	abbr --add gp 'git push'
+	abbr --add gs 'git status'
+
+	# Neovim
+	abbr --add v 'nvim'
+	abbr --add vim 'nvim'
+
+	# Feral programs
+	abbr --add n 'feral todo'
+	abbr --add c 'feral build-project'
+
+	# Stuff to run
 	neofetch
 	if type -q feral
-		n l
+		feral todo l
 		echo
 	end
 end
